8b55a0d8b3ab06e9535a7058df503e5e16529afa
Luna Simons <luna@bddvlpr.com>
2023-10-17 11:58:50 +0200

    chore: project setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,52 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/strict"],
+  plugins: ["@typescript-eslint", "import", "prettier"],
+  rules: {
+    "@typescript-eslint/consistent-indexed-object-style": "off", // sometimes naming keys is more user-friendly
+    "@typescript-eslint/no-dynamic-delete": "off", // delete is OK
+    "@typescript-eslint/no-non-null-assertion": "off", // this is better than "as"
+    "@typescript-eslint/no-shadow": "error",
+    "@typescript-eslint/no-unnecessary-condition": "off", // this gives bad advice
+    "arrow-body-style": ["error", "as-needed"],
+    "dot-notation": "error",
+    "import/newline-after-import": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+          orderImportKind: "asc",
+          caseInsensitive: true,
+        },
+        groups: [
+          ["builtin", "external"],
+          "internal",
+          "parent",
+          "index",
+          "sibling",
+        ],
+      },
+    ],
+    curly: "error",
+    "object-shorthand": "error", // don’t use foo["bar"]
+    "no-console": "error",
+    "no-global-assign": "error",
+    "no-undef": "off", // handled by TS
+    "no-unused-vars": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.test.*"],
+      rules: {
+        "@typescript-eslint/ban-ts-comment": "off", // allow @ts-ignore only in tests
+        "@typescript-eslint/no-empty-function": "off", // don’t enforce this in tests
+        "@typescript-eslint/no-explicit-any": "off", // tests sometimes need this
+      },
+    },
+  ],
+};
